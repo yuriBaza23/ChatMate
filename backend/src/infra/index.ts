ef6698d2bd27babd0dfc3d9b9d5d7a86d7141d9b
yuriBaza23 <yuribaza2321@gmail.com>
2023-04-07 14:46:11 -0300

    feat: create a websocket service

--- backend/src/infra/index.ts
+++ backend/src/infra/index.ts
@@ -2,12 +2,18 @@ import express from "express";
 import http from "http"; 
 import cors from "cors";
 import { appRoutes } from "./routes/app.routes";
+import { WebSocketAPI } from "./websockets";
 
 const app = express();
 app.use(cors());
-
 const server = http.createServer(app);
 
+// Socket
+const webSocket = new WebSocketAPI(server);
+webSocket.execute();
+
+// Routes
 app.get("/", appRoutes);
 
+// Listen
 server.listen(3333, () => console.log("Server is running on port 3333"));
